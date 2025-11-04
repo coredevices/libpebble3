88ce4246dbfd241511a9253e7914a092e8ce5457
crc-32 <me@crc32.dev>
2025-11-04 01:08:31 +0000

    handle app not declaring PebbleKit provider

--- libpebble3/src/androidMain/kotlin/io/rebble/libpebblecommon/pebblekit/PebbleKitProviderNotifier.kt
+++ libpebble3/src/androidMain/kotlin/io/rebble/libpebblecommon/pebblekit/PebbleKitProviderNotifier.kt
@@ -1,6 +1,7 @@
 package io.rebble.libpebblecommon.pebblekit
 
 import android.content.Context
+import co.touchlab.kermit.Logger
 import io.rebble.libpebblecommon.connection.ConnectedPebbleDevice
 import io.rebble.libpebblecommon.connection.LibPebble
 import io.rebble.libpebblecommon.connection.Watches
@@ -11,6 +12,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class PebbleKitProviderNotifier : LibPebbleKoinComponent {
+    companion object {
+        private val logger = Logger.withTag("PebbleKitProviderNotifier")
+    }
     fun init() {
         val watches: Watches = getKoin().get<LibPebble>()
         val libPebbleCoroutineScope: LibPebbleCoroutineScope = getKoin().get()
@@ -22,7 +26,11 @@ class PebbleKitProviderNotifier : LibPebbleKoinComponent {
             }
                 .distinctUntilChanged()
                 .collect {
-                    context.contentResolver.notifyChange(PebbleKitProvider.URI_CONTENT_BASALT, null)
+                    try {
+                        context.contentResolver.notifyChange(PebbleKitProvider.URI_CONTENT_BASALT, null)
+                    } catch (e: SecurityException) {
+                        logger.e(e) { "Failed to notify PebbleKitProvider content change - is the provider present in app manifest?" }
+                    }
                 }
         }
     }
